1367397dbe7c6ea439502c7bbcfa9ee6b4fd08a0
gongph <gongph@foxmail.com>
2017-07-10 17:41:15 +0800

    add movie module

---
+++ src/store/modules/movie.js
@@ -0,0 +1,42 @@
+import server from '../../models/movie.js';
+import * as types from '../mutation-types.js';
+
+const state = {
+  topten: []
+}
+
+const getters = {
+  topTen: state => state.topten
+}
+
+const actions = {
+  getTopTen ({ commit }) {
+    server.getToptenMovie().then(response => {
+      commit(types.RECEIVE_TOPTEN, response.data.data);
+    });
+  },
+  getNextPageById ({ commit }, id) {
+    server.getNextPageById(id).then(response => {
+      commit(types.RECEIVE_NEXTPAGE, response.data.data);
+    });
+  }
+}
+
+const mutations = {
+  [types.RECEIVE_TOPTEN] (state, data) {
+  	state.topten.length = 0;
+    state.topten = data;
+  },
+  [types.RECEIVE_NEXTPAGE] (state, data) {
+  	state.topten = state.topten.concat(data);
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
+
