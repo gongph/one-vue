abfe92d02e49ee79fb85828ddee0c131a10ca6b5
gongph <gongph@foxmail.com>
2017-07-10 17:40:46 +0800

    add music model

---
+++ src/models/music.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import api from '../api';
+
+const instance = axios.create({
+	baseURL: api.baseURL
+});
+
+const reg = /\{(.+)\}/ig;
+
+export default {
+  getToptenMusic () {
+    return instance.get(api.getToptenMusic);
+  },
+  getNextPageById (id) {
+    return instance.get(api.getNextMusic.replace(reg, id));
+  }
+}
