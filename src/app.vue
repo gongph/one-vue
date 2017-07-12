201f93950b50112ce5b308384668de49419c83ed
gongph <gongph@foxmail.com>
2017-07-12 16:56:55 +0800

    modified toolbar active state

--- src/app.vue
+++ src/app.vue
@@ -6,10 +6,10 @@
       <f7-view main tab active url="/home/"></f7-view>
       
       <f7-toolbar tabbar class="global-toolbar">
-        <f7-link @click="loadpage('/home/')" tab-link active no-link-class icon-f7="home"></f7-link>
-        <f7-link @click="loadpage('/reading/')" tab-link no-link-class icon-f7="book"></f7-link>
-        <f7-link @click="loadpage('/music/')" tab-link no-link-class icon-f7="tune"></f7-link>
-        <f7-link @click="loadpage('/movie/')" tab-link no-link-class icon-f7="videocam"></f7-link>
+        <f7-link @click="loadpage($event, '/home/')" tab-link active no-link-class icon-f7="home"></f7-link>
+        <f7-link @click="loadpage($event, '/reading/')" tab-link no-link-class icon-f7="book"></f7-link>
+        <f7-link @click="loadpage($event, '/music/')" tab-link no-link-class icon-f7="tune"></f7-link>
+        <f7-link @click="loadpage($event, '/movie/')" tab-link no-link-class icon-f7="videocam"></f7-link>
       </f7-toolbar>
     </f7-views>
   </div>
@@ -18,7 +18,9 @@
 <script>
   export default {
     methods: {
-      loadpage (page) {
+      loadpage (event, page) {
+      	this.$root.$$('.global-toolbar .tab-link').removeClass('active');
+      	this.$root.$$(event.target).closest('a').addClass('active');
         this.$root.$f7.getCurrentView().router.loadPage({
         	url: page,
         	animatePages: false
