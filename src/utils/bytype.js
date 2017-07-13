49b1f43a6d21059916fdf97d98604d2013c2e215
gongph <gongph@foxmail.com>
2017-07-13 17:37:26 +0800

    format bytype.js file

--- src/utils/bytype.js
+++ src/utils/bytype.js
@@ -1,17 +1,17 @@
 import api from '../api';
 
 /**
- * 根据 type 获取英文分类
- * @param {String} type 分类
+ * 根据 type 获取路由url
+ * @param {String} type 文章类型
  */
 
 const getRouterByType = function (type) {
   const map = {
-  	'1': '/reading/essay/',
-  	'2': '/reading/serial/',
-  	'3': '/reading/question/',
-  	'4': '/music/',
-  	'5': '/movie/'
+    '1': '/reading/essay/',
+    '2': '/reading/serial/',
+    '3': '/reading/question/',
+    '4': '/music/',
+    '5': '/movie/'
   }
   return type ? map[type] : '';
 }
@@ -20,17 +20,17 @@ export { getRouterByType };
 
 /**
  * 根据 type 获取分类
- * @param {String} type 分类
+ * @param {String} type 文章类型
  */
 
 const getCategoryByType = function (type) {
   const map = {
-  	'0': '插画',
-  	'1': 'ONE STORY',
-  	'2': '连载',
-  	'3': '问答',
-  	'4': '音乐',
-  	'5': '影视'
+    '0': '插画',
+    '1': 'ONE STORY',
+    '2': '连载',
+    '3': '问答',
+    '4': '音乐',
+    '5': '影视'
   }
   return type ? map[type] : '';
 }
