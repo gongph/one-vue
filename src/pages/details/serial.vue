b4be0fa216f780748bad21e1514c74831c4d767f
gongph <gongph@foxmail.com>
2017-07-13 15:28:17 +0800

    new add serial.vue page

---
+++ src/pages/details/serial.vue
@@ -0,0 +1,22 @@
+<template>
+  <f7-page name="serial" class="serial-page" 
+  	navbar-through 
+  	tabbar-through>
+  	<!-- navbar -->
+    <f7-navbar>
+    	<f7-nav-left>
+    		<f7-link back icon-f7="left" color="gray"></f7-link>
+    	</f7-nav-left>
+      <f7-nav-center sliding>阅读·连载</f7-nav-center>
+      <f7-nav-right>
+      	<f7-link icon-f7="share" color="gray"></f7-link>
+      </f7-nav-right>
+    </f7-navbar>
+    
+  </f7-page>
+  
+</template>
+
+<script>
+  export default {}
+</script>
