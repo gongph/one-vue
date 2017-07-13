7b664d292892b37f691b868e842c53be20eb215e
gongph <gongph@foxmail.com>
2017-07-13 16:22:47 +0800

    format details/serial.vue

--- src/pages/details/serial.vue
+++ src/pages/details/serial.vue
@@ -5,8 +5,8 @@
 
     <!-- navbar -->
     <f7-navbar>
-    	<f7-nav-left>
-    	  <f7-link back icon-f7="left" color="gray"></f7-link>
+      <f7-nav-left>
+        <f7-link back icon-f7="left" color="gray"></f7-link>
     	</f7-nav-left>
       <f7-nav-center sliding>阅读·连载</f7-nav-center>
       <f7-nav-right>
@@ -23,27 +23,27 @@
           <div class="hp_author size-16">文╱{{ this.author.user_name }}</div>
           <div class="hp_content size-16" v-html="this.serial.content"></div>
           <template v-if="this.serial.charge_edt ">
-      		  <div class="hp_author_introduce size-12 color-gray">
-      				{{ this.serial.charge_edt.replace(/（|）/g, '') }}
-      		  </div>
-      		</template>
+            <div class="hp_author_introduce size-12 color-gray">
+              {{ this.serial.charge_edt.replace(/（|）/g, '') }}
+            </div>
+          </template>
       		
           <!-- 作者 -->
           <div class="author_info">
             <div class="at_title">作者</div>
             <div class="at_split"></div>
             <ul class="at_content">
-      		  	<li class="web_url">
-      		  	  <img :src="this.author.web_url" width="32" height="32"/>
-      		  	</li>
-      		  	<li class="wb_name">
-      		  		<span>{{ this.author.user_name }} {{ this.author.wb_name }}</span>
-      		  		<small class="size-12 color-gray">{{ this.author.summary }}</small>
-      		  	</li>
-      		  	<li class="follow">
-      		  		<f7-button color="gray" class="size-12">关注</f7-button>
-      		  	</li>
-      		  </ul>
+              <li class="web_url">
+                <img :src="this.author.web_url" width="32" height="32"/>
+              </li>
+              <li class="wb_name">
+                <span>{{ this.author.user_name }} {{ this.author.wb_name }}</span>
+                <small class="size-12 color-gray">{{ this.author.summary }}</small>
+              </li>
+              <li class="follow">
+                <f7-button color="gray" class="size-12">关注</f7-button>
+              </li>
+            </ul>
           </div>
         </div>
       </f7-col>
@@ -70,8 +70,8 @@
 </template>
 
 <script>
-	import Mixins from '../../mixins/mixins.vue';
-	import { mapGetters, mapActions } from 'vuex';
+  import Mixins from '../../mixins/mixins.vue';
+  import { mapGetters, mapActions } from 'vuex';
   export default {
     mixins: [Mixins],
     computed: {
