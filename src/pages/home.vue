7734c76b46e3f99c24395923b6c661f70f469bd6
gongph <gongph@foxmail.com>
2017-07-13 15:29:04 +0800

    modified router of home.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,10 +2,11 @@
   <f7-page ref="homepage" name="home" class="home-page" 
   	navbar-through 
   	tabbar-through>
+  	
     <!-- navbar -->
     <f7-navbar v-if="showNavbar">
     	<f7-nav-left>
-    		<f7-link icon-f7="person" color="gray"></f7-link>
+    	  <f7-link icon-f7="person" color="gray"></f7-link>
     	</f7-nav-left>
       <f7-nav-center sliding>一个</f7-nav-center>
       <f7-nav-right>
@@ -16,8 +17,8 @@
     <!-- title -->
     <f7-grid>
     	<f7-col width="100">
-    		<div class="weather">
-    			<div class="date size-17">{{ this.curDate }}</div>
+    	  <div class="weather">
+    		  <div class="date size-17">{{ this.curDate }}</div>
     		  <div class="climate size-12">{{ this.climate }}</div>
     		</div>
     	</f7-col>
@@ -26,8 +27,8 @@
     <!-- poster -->
     <f7-grid>
     	<f7-col width="100">
-    		<div class="poster">
-    			<div class="volume size-12">{{ this.poster.volume }}</div>
+    	  <div class="poster">
+    		  <div class="volume size-12">{{ this.poster.volume }}</div>
     			<div class="img_url">
     				<img :src="this.poster.img_url" width="100%" class="lazy lazy-fadein"/>
     			</div>
@@ -41,14 +42,14 @@
     				{{ this.poster.words_info }}
     			</div>
     		</div>
-    		<div class="footerbar note">
+    	  <div class="footerbar note">
     			<f7-link no-link-class>
     			  <f7-icon f7="compose"></f7-icon> 小记
     			</f7-link>
     			<f7-link icon-f7="share"></f7-link>
     			<f7-link>
-    				{{ this.poster.like_count }}
-    				<f7-icon f7="heart"></f7-icon>
+    			  {{ this.poster.like_count }}
+    			  <f7-icon f7="heart"></f7-icon>
     			</f7-link>
     		</div>
     	</f7-col>
@@ -57,9 +58,9 @@
     <!-- nodes -->
     <f7-grid class="node-row" v-for="(node, index) in this.nodes" v-if="index != 0" key="node.id">
       <f7-col width="100">
-        <div class="node">
+        <div class="node" @click="loadDetailPage(node)">
         	<div class="header">
-        	  <div class="category color-gray size-12">- {{ getTag(node.tag_list) }} -</div>
+        	  <div class="category color-gray size-12">- {{ getTag(node) }} -</div>
         	  <div class="title size-20">{{ node.title }}</div>
         	  <div class="author color-gray size-14">文╱{{getUserName(node.author)}}</div>
         	</div>
@@ -71,7 +72,7 @@
         	  <f7-link no-link-class>一天前</f7-link>
         	  <f7-link icon-f7="share"></f7-link>
         	  <f7-link>
-        	  	{{ node.like_count }}
+        	    {{ node.like_count }}
         	  	<f7-icon f7="heart"></f7-icon>
         	  </f7-link>
         	</div>
@@ -83,35 +84,51 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-import axios from 'axios';
-export default {
-	data () {
-	  showNavbar: false
-	},
-	computed: {
-		...mapGetters('home', [
-		  'todaydate',
-		  'climate',
-		  'poster',
-		  'nodes'
-	  ]),
-	  curDate () {
-	  	return this.todaydate.replace(/-/g, ' / ');
-	  }
-	},
-	created () {
-		this.getIdlist();
-	},
-	methods: {
-		...mapActions('home', [
-		  'getIdlist'
-		]),
-    getUserName (author) {
-  	  return author? author.user_name : '';
+  import Mixins from '../mixins/mixins.vue';
+  import { mapGetters, mapActions } from 'vuex';
+  import { getRouterByType, getCategoryByType } from '../utils/bytype.js';
+  export default {
+    mixins: [Mixins],
+    data () {
+	    showNavbar: false
+	  },
+	  computed: {
+		  ...mapGetters('home', [
+		    'todaydate',
+		    'climate',
+		    'poster',
+		    'nodes'
+	    ]),
+	    curDate () {
+	  	  return this.todaydate.replace(/-/g, ' / ');
+	    }
+	  },
+	  created () {
+		  this.getIdlist();
+	  },
+	  methods: {
+		  ...mapActions('home', [
+		    'getIdlist'
+		  ]),
+      getUserName (author) {
+  	    return author? author.user_name : '';
+      },
+      getTag (node) {
+  	    let taglist = node.tag_list, tag = '';
+  	    if (taglist.length > 0) {
+  	  	  tag = taglist[0].title;
+  	    } else {
+  	      tag = getCategoryByType(node.category);
+  	    }
+  	    return tag;
     },
-    getTag (tagList) {
-  	  return (tagList.length > 0) ? tagList[0].title : '阅读';
+    loadDetailPage (node) {
+    	this.hideToolbar();
+    	let router = getRouterByType(node.category);
+      this.$root.$f7.getCurrentView().router.loadPage({ 
+    	  url: router + node.item_id,
+    	  animatePages: false
+    	});
     }
   }
 }
