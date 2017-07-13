17fac5820743703ee7dd0b509a01fdfdeec31c09
gongph <gongph@foxmail.com>
2017-07-13 15:25:12 +0800

    add new function getCategoryByType

--- src/utils/bytype.js
+++ src/utils/bytype.js
@@ -1,16 +1,38 @@
 import api from '../api';
 
-const getCategoryById = function (type) {
+/**
+ * 根据 type 获取英文分类
+ * @param {String} type 分类
+ */
+
+const getRouterByType = function (type) {
   const map = {
-  	'0': 'hp',
-  	'1': 'essay',
-  	'2': 'serial',
-  	'3': 'question',
-  	'4': 'music',
-  	'5': 'movie'
+  	'1': '/reading/essay/',
+  	'2': '/reading/serial/',
+  	'3': '/reading/question/',
+  	'4': '/music/',
+  	'5': '/movie/'
   }
-  
   return type ? map[type] : '';
 }
 
-export { getCategoryById };
+export { getRouterByType };
+
+/**
+ * 根据 type 获取分类
+ * @param {String} type 分类
+ */
+
+const getCategoryByType = function (type) {
+  const map = {
+  	'0': '插画',
+  	'1': 'ONE STORY',
+  	'2': '连载',
+  	'3': '问答',
+  	'4': '音乐',
+  	'5': '影视'
+  }
+  return type ? map[type] : '';
+}
+
+export { getCategoryByType };
