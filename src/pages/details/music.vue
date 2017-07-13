00db3c38bc8a4542936adbfa3874691cf9f6564a
gongph <gongph@foxmail.com>
2017-07-13 16:40:32 +0800

    format details/music.vue

--- src/pages/details/music.vue
+++ src/pages/details/music.vue
@@ -17,41 +17,42 @@
     <f7-grid class="article-row">
       <f7-col width="100">
       	<div class="article">
-      		<div class="hp_m_cover">
-      			<img :src="this.article.cover" width="100%"/>
-      		</div>
-      		<div class="hp_m_title color-gray size-14">
-      		  <p>· {{ this.article.title }} ·</p>
-      		  <p>{{ this.author.user_name }} | {{ this.article.album}}</p>
-      		</div>
-      		<!--<div class="hp_mc_author color-gray size-14"></div>-->
-      		<div class="hp_m_story_title size-25">{{ this.article.story_title }}</div>
-      		<div class="hp_m_story_author">
-      		  <p>文╱{{ this.storyAuthor.user_name}}</p>
-      		</div>
-      		<div class="hp_content size-16" v-html="this.article.story"></div>
-      		<template v-if="this.article.charge_edt">
-      		  <div class="hp_author_introduce size-12 color-gray">
-      				{{ this.article.charge_edt.replace(/（|）/g, '') }}
-      		  </div>
-      		</template>
-      		<!-- 作者 -->
-      		<div class="author_info">
-      		  <div class="at_title">作者</div>
-      		  <div class="at_split"></div>
-      		  <ul class="at_content">
-      		  	<li class="web_url">
-      		  	  <img :src="this.storyAuthor.web_url" width="32" height="32"/>
-      		  	</li>
-      		  	<li class="wb_name">
-      		  		<span>{{ this.storyAuthor.user_name }} {{ this.storyAuthor.wb_name }}</span>
-      		  		<small class="size-12 color-gray">{{ this.storyAuthor.summary }}</small>
-      		  	</li>
-      		  	<li class="follow">
-      		  		<f7-button color="gray" class="size-12">关注</f7-button>
-      		  	</li>
-      		  </ul>
-      		</div>
+      	  <div class="hp_m_cover">
+      	    <img :src="this.article.cover" width="100%"/>
+      	  </div>
+      	  <div class="hp_m_title color-gray size-14">
+      	    <p>· {{ this.article.title }} ·</p>
+      	    <p>{{ this.author.user_name }} | {{ this.article.album}}</p>
+      	  </div>
+      	  <!--<div class="hp_mc_author color-gray size-14"></div>-->
+      	  <div class="hp_m_story_title size-25">{{ this.article.story_title }}</div>
+      	  <div class="hp_m_story_author">
+      	    <p>文╱{{ this.storyAuthor.user_name}}</p>
+      	  </div>
+      	  <div class="hp_content size-16" v-html="this.article.story"></div>
+      	  <template v-if="this.article.charge_edt">
+      	    <div class="hp_author_introduce size-12 color-gray">
+      	      {{ this.article.charge_edt.replace(/（|）/g, '') }}
+      	    </div>
+      	  </template>
+      	  
+      	  <!-- 作者 -->
+      	  <div class="author_info">
+      	    <div class="at_title">作者</div>
+      	    <div class="at_split"></div>
+      	    <ul class="at_content">
+      	      <li class="web_url">
+      	        <img :src="this.storyAuthor.web_url" width="32" height="32"/>
+      	      </li>
+      	      <li class="wb_name">
+      	        <span>{{ this.storyAuthor.user_name }} {{ this.storyAuthor.wb_name }}</span>
+      	        <small class="size-12 color-gray">{{ this.storyAuthor.summary }}</small>
+      	      </li>
+      	      <li class="follow">
+      	        <f7-button color="gray" class="size-12">关注</f7-button>
+      	      </li>
+      	    </ul>
+      	  </div>
       	</div>
       </f7-col>
     </f7-grid>
@@ -79,7 +80,7 @@
   import Mixins from '../../mixins/mixins.vue';
   import { mapGetters, mapActions } from 'vuex';
   export default {
-  	mixins: [Mixins],
+    mixins: [Mixins],
     computed: {
       ...mapGetters('music', [
         'article',
