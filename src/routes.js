ff263e7e5623d743a2a1722f7f5e66ea345352aa
gongph <gongph@foxmail.com>
2017-07-13 15:30:05 +0800

    new add serial and question router

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Music from './pages/musics.vue';
 import Movie from './pages/movies.vue';
 
 import EssayDetail from './pages/details/essay.vue';
+import SerialDetail from './pages/details/serial.vue';
+import QuestionDetail from './pages/details/question.vue';
 import MusicDetail from './pages/details/music.vue';
 import MovieDetail from './pages/details/movie.vue';
 
@@ -19,6 +21,14 @@ export default [
   {
 	  path: '/reading/essay/:id',
 	  component: EssayDetail
+  },
+  {
+  	path: '/reading/serial/:id',
+  	component: SerialDetail
+  },
+  {
+  	path: '/reading/question/:id',
+  	component: QuestionDetail
   },
   {
 	  path: '/music/',
