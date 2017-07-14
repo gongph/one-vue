95fa699122c67ec0bbc9be3202d57a0187a7b361
gongph <gongph@foxmail.com>
2017-07-14 12:16:42 +0800

    modified some methods

--- src/mixins/mixins.vue
+++ src/mixins/mixins.vue
@@ -1,21 +1,35 @@
 <script>
   export default {
     methods: {
-      // 隐藏工具栏
+      /**
+       * 得到当前视图
+       */
+      getCurrentView () {
+        return this.$root.$f7.getCurrentView();
+      },
+      /**
+       * 隐藏工具栏
+       */
       hideToolbar () {
         this.$root.$$('.global-toolbar').hide();
       },
-      // 显示工具栏
+      /**
+       * 显示工具栏
+       */
       showToolbar () {
       	this.$root.$$('.global-toolbar').show();
       },
-      // 隐藏加载指示器
-      hideIndicator () {
-      	this.$root.$f7.hideIndicator();
+      /**
+       * 隐藏导航栏
+       */
+      hideNavbar () {
+      	this.getCurrentView().hideNavbar();
       },
-      // 显示加载指示器
-      showIndicator () {
-      	this.$root.$f7.showIndicator();
+      /**
+       * 显示导航栏
+       */
+      showNavbar () {
+      	this.getCurrentView().showNavbar();
       }
     }
   }
